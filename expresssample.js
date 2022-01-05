5f65d3ae033475360ae6883d21208aa3b8d19758
PranavBala1920 <81830566+PranavBala1920@users.noreply.github.com>
2022-01-05 17:41:54 +0530

    Update expresssample.js

--- expresssample.js
+++ expresssample.js
@@ -1,14 +1,4 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.sendFile('./sample.html')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
